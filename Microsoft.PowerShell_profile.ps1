819d22a7281d499f4496c07478fcc77476cc2630
Phuong Nguyen <artphuong@live.com>
2018-02-11 11:55:36 -0800

    Added commands for WordPress and directory transversal

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -27,3 +27,27 @@ function GitBranch {
     & git branch $args
 }
 New-Alias -Name gitb -Value GitBranch
+
+# WordPress functions
+function WordPressPlugins {
+    & cd .\wp-content\plugins
+}
+New-Alias -Name cdwpp -Value WordPressPlugins
+
+function WordPressThemes {
+    & cd .\wp-content\themes
+}
+New-Alias -Name cdwpt -Value WordPressThemes
+
+# Directory transversal functions
+
+# Invoke the up commands to travel up directories. 
+for($i = 1; $i -le 10; $i++) {
+    $up = "".PadLeft($i,"u")
+    $upnum = "up$i"
+    $d = $up.Replace("u","../")
+    Invoke-Expression "function $up { push-location $d }"
+    Invoke-Expression "function $upnum { push-location $d }"
+}
+Invoke-Expression "function up { push-location '../' }"
+
