1a680d44b5236b0742bbea4baa1833639285a06c
Phuong Nguyen <artphuong@live.com>
2018-02-19 11:09:44 -0800

    Added if/else statements for New-Alias and Set-Alias to prevents duplicate alias errors on profile reload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,11 @@
-# Quickly edit this profile
 function EditProfile {
-    & c:
     & powershell_ise.exe $profile
 }
-New-Alias -Name pse -Value EditProfile
+if (!(Test-Path alias:pse)) { 
+    New-Alias -Name pse -Value EditProfile 
+} else {
+    Set-Alias -Name pse -Value EditProfile
+}
 
 function ReloadProfile {
     @(
@@ -18,61 +20,138 @@ function ReloadProfile {
         }
     } 
 }
-New-Alias -Name psr -Value ReloadProfile
+if (!(Test-Path alias:psr)) { 
+    New-Alias -Name psr -Value ReloadProfile 
+} else {
+    Set-Alias -Name psr -Value ReloadProfile
+}
+
+# Import PSColor module. This customizes the colors of the directory listing
+Import-Module PSColor
 
 # Git functions
 function GitStatus { 
     & git status $args
 }
-New-Alias -Name s -Value GitStatus
+
+if (!(Test-Path alias:s)) { 
+    New-Alias -Name s -Value GitStatus 
+} else {
+    Set-Alias -Name s -Value GitStatus
+}
 
 function GitAddAll {
     & git add .
 }
-New-Alias -Name aa -Value GitAddAll
+if (!(Test-Path alias:aa)) { 
+    New-Alias -Name aa -Value GitAddAll 
+} else {
+    Set-Alias -Name aa -Value GitAddAll
+}
 
 function GitCommit { 
     & git commit -m $args 
 }
-New-Alias -Name c -Value GitCommit
+if (!(Test-Path alias:c)) { 
+    New-Alias -Name c -Value GitCommit 
+} else {
+    Set-Alias -Name c -Value GitCommit
+}
 
 function GitPushOrigin {
     & git push origin $args
 }
-New-Alias -Name p -Value GitPushOrigin
+if (!(Test-Path alias:p)) { 
+    New-Alias -Name p -Value GitPushOrigin 
+} else {
+    Set-Alias -Name p -Value GitPushOrigin
+}
+
+function GitPushOriginMaster {
+    & git push origin master
+}
+if (!(Test-Path alias:pm)) { 
+    New-Alias -Name pm -Value GitPushOriginMaster 
+} else {
+    Set-Alias -Name pm -Value GitPushOriginMaster
+}
 
 function GitPullOrigin {
     & git pull origin $args
 }
-New-Alias -Name pl -Value GitPullOrigin
+if (!(Test-Path alias:pl)) { 
+    New-Alias -Name pl -Value GitPullOrigin 
+} else {
+    Set-Alias -Name pl -Value GitPullOrigin
+}
+
+function GitPullOriginMaster {
+    & git pull origin master
+}
+if (!(Test-Path alias:pl)) { 
+    New-Alias -Name plm -Value GitPullOriginMaster 
+} else {
+    Set-Alias -Name plm -Value GitPullOriginMaster
+}
 
 function GitCheckout {
     & git co $args
 }
-New-Alias -Name co -Value GitCheckout
+Set-Alias -Name co -Value GitCheckout
+if (!(Test-Path alias:co)) { 
+    New-Alias -Name co -Value GitCheckout 
+} else {
+    Set-Alias -Name co -Value GitCheckout
+}
 
 function GitBranch {
     & git branch $args
 }
-New-Alias -Name b -Value GitBranch
+if (!(Test-Path alias:b)) { 
+    New-Alias -Name b -Value GitBranch 
+} else {
+    Set-Alias -Name b -Value GitBranch
+}
 
 function GitMerge {
     & git merge $args
 }
-New-Alias -Name m -Value GitMerge
+if (!(Test-Path alias:m)) { 
+    New-Alias -Name m -Value GitMerge 
+} else {
+    Set-Alias -Name m -Value GitMerge
+}
 
 
 
 # WordPress functions
+function WordPressDirectory {
+    & d:
+    & cd D:\xampp\htdocs\wordpress
+}
+if (!(Test-Path alias:cdwp)) { 
+    New-Alias -Name cdwp -Value WordPressDirectory 
+} else {
+    Set-Alias -Name cdwp -Value WordPressDirectory
+}
+
 function WordPressPlugins {
     & cd .\wp-content\plugins
 }
-New-Alias -Name cdwpp -Value WordPressPlugins
+if (!(Test-Path alias:cdwpp)) { 
+    New-Alias -Name cdwpp -Value WordPressPlugins 
+} else {
+    Set-Alias -Name cdwpp -Value WordPressPlugins
+}
 
 function WordPressThemes {
     & cd .\wp-content\themes
 }
-New-Alias -Name cdwpt -Value WordPressThemes
+if (!(Test-Path alias:cdwpt)) { 
+    New-Alias -Name cdwpt -Value WordPressThemes 
+} else {
+    Set-Alias -Name cdwpt -Value WordPressThemes
+}
 
 
 
